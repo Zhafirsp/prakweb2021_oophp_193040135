948c39e75e756ef7fb5b7eb44320efa6a3b3a2ce
Zhafir <dsquaredea@gmail.com>
2021-09-17 13:42:04 +0700

    Mempelajari tentang Class dan Objek pada video oop dasar pada php 2

---
+++ oophp/Pertemuan 2/coba.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Muhammad Zhafir Sunandy Pramana
+193040135
+https://github.com/Zhafirsp/prakweb2021_oophp_193040135.git
+Pertemuan 2 - 17 September 2021
+Mempelajari tentang Class dan Objek
+*/
+?>
+<?php
+
+class Coba
+{
+}
+
+// membuat object instance dari class
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+?>
