4b690bd4ffd1d241a4a24798232e66d10f297dc9
Zhafir <dsquaredea@gmail.com>
2021-09-17 14:13:25 +0700

    Mempelajari tentang constructor pada video oop dasar pada php 4

---
+++ oophp/Pertemuan 2/constructor.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Muhammad Zhafir Sunandy Pramana
+193040135
+https://github.com/Zhafirsp/prakweb2021_oophp_193040135.git
+Pertemuan 2 - 17 September 2021
+Mempelajari tentang Constructor
+*/
+
+use Produk as GlobalProduk;
+
+?>
+
+<?php
+
+class Produk
+{
+  public  $judul, $penulis, $penerbit, $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 2500000);
+$produk3 = new Produk("Dragon Ball");
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
